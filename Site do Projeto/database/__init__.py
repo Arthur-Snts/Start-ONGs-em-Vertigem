00dddef02c358c8e1aa906ab916ff48b587f6c7a
JulioTVRS <151770679+JulioTVRS@users.noreply.github.com>
2024-11-27 17:09:28 -0300

    Removendo o site

--- Site do Projeto/database/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app):
-    app.config.from_object('database.config.Config')
-    db.init_app(app)
--- Site do Projeto/database/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = "superdificil"
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/databasepi'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
